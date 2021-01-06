60fa85bdbfe508d42654940ba9c8ce3780aad26a
Smendowski <m.smendowski@gmail.com>
2021-01-06 12:52:54 +0100

    Fotter and Grafana-Map connecting
    - IFrame installation
    - basic footer template
    - Responsive grafana's map connection

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import styles from './Footer.module.css';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://g01.labagh.pl/">
+        Coronavirus COVID-19 statistics
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    position: 'relative'
+  },
+}));
 
 const Footer = () => {
-    return (
-        <div className={styles.footer}>
-            &copy; {new Date().getFullYear()} Mateusz Smendowski  Nikita Muravytskyi Krzysztof Skoś   
-        </div>
-    )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body3">Some content</Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Iframe from 'react-iframe';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    map: {
+        width: '99vw',
+        height: '90vh',
+        position: 'absolute',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    }
+}));
 
 const Map = () => {
-    return <div>This is the page with a map</div>
+    const classes = useStyles();
+    return <div>
+        <Iframe className={ classes.map } 
+            url="http://bazy.labagh.pl:3001/d-solo/wF8hSd-Gk/new-dashboard-copy?orgId=1&from=1609769590748&to=1609791190749&panelId=2"
+        />
+    </div>
 };
 
 export default Map;
