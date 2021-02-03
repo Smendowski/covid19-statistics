af813971044b0eb20a6d108d2f7e864c9a804dc2
mmuravytskyi <purchasedinukrain@gmail.com>
2021-02-03 13:58:46 +0100

    done

--- frontend/src/components/Newsletter.js
+++ frontend/src/components/Newsletter.js
@@ -37,7 +37,7 @@ class Newsletter extends React.Component {
           body: JSON.stringify({ 'email': this.state.input.email, 'subscriptions': (localStorage.getItem('country') ? localStorage.getItem('country') : 'Poland') })
         }
 
-        fetch('http://g01.labagh.pl:4000/api/newsletter', requestOptions)
+        fetch('https://g01.labagh.pl/api/api/newsletter', requestOptions)
     }
   }
   
